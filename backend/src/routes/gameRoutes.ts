7fc5edd9f3ed4047808efb400536f29f707ec905
Callum Edwards <126828563+Calx03@users.noreply.github.com>
2026-03-12 22:22:46 +0000

    added a route to fetch game details by RawgID

--- backend/src/routes/gameRoutes.ts
+++ backend/src/routes/gameRoutes.ts
@@ -2,12 +2,28 @@ import { Router } from "express";
 
 const router = Router();
 
+// Test RawgID: 3498 (GTA V)
+
 router.get("/search", async (req, res) => {
   const { query } = req.query;
 
   try {
     const response = await fetch(
       `https://api.rawg.io/api/games?search=${query}&key=${process.env.RAWG_KEY}`,
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.get("/:rawgId", async (req, res) => {
+  const { rawgId } = req.params;
+
+  try {
+    const response = await fetch(
+      `https://api.rawg.io/api/games/${rawgId}?key=${process.env.RAWG_KEY}`,
     );
     const data = await response.json();
     res.json(data);
