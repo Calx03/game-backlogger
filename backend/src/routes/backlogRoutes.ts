d2f08ea2a089966074ab326c7b562248f9696b46
Callum Edwards <126828563+Calx03@users.noreply.github.com>
2026-03-13 21:40:02 +0000

    Add validation for existing backlog entries and implement update and delete functionality

--- backend/src/routes/backlogRoutes.ts
+++ backend/src/routes/backlogRoutes.ts
@@ -4,7 +4,7 @@
 import { Router } from "express";
 import { db } from "../db/db.js";
 import { BacklogEntryTable, UserTable } from "../db/schema.js";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 
 const router = Router();
 
@@ -31,12 +31,72 @@ router.post("/", async (req, res) => {
   const { rawgId, status, rating, notes } = req.body;
 
   try {
+    const existingEntry = await db
+      .select()
+      .from(BacklogEntryTable)
+      .where(
+        and(
+          eq(BacklogEntryTable.rawgId, rawgId),
+          eq(BacklogEntryTable.userId, req.user.id),
+        ),
+      );
+
+    if (existingEntry.length > 0) {
+      return res.json({ error: "Game already in backlog" });
+    }
+
     const entry = await db
       .insert(BacklogEntryTable)
       .values({ rawgId, status, rating, notes, userId: req.user.id })
       .returning();
 
     res.json(entry);
+  } catch (err: any) {
+    console.log(err.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  if (!req.user) return res.status(401).json({ error: "Unauthorised" });
+
+  const { status, rating, notes } = req.body;
+  const id = parseInt(req.params.id);
+  try {
+    const updatedEntry = await db
+      .update(BacklogEntryTable)
+      .set({ status, rating, notes })
+      .where(
+        and(
+          eq(BacklogEntryTable.id, id),
+          eq(BacklogEntryTable.userId, req.user.id),
+        ),
+      )
+      .returning();
+
+    res.json(updatedEntry);
+  } catch (err: any) {
+    console.log(err.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  if (!req.user) return res.status(401).json({ error: "Unauthorised" });
+
+  const id = parseInt(req.params.id);
+
+  try {
+    await db
+      .delete(BacklogEntryTable)
+      .where(
+        and(
+          eq(BacklogEntryTable.id, id),
+          eq(BacklogEntryTable.userId, req.user.id),
+        ),
+      );
+
+    res.status(204).send();
   } catch (err: any) {
     console.log(err.message);
     res.status(500).json({ error: "Internal server error" });
